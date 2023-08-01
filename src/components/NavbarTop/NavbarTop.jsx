15cc46da6790444dd4157b4e11839c49d8f9544b
Adrian Litewski <litewskidev@gmail.com>
2023-08-01 20:55:19 +0200

    add top navbar

--- src/components/NavbarTop/NavbarTop.jsx
+++ src/components/NavbarTop/NavbarTop.jsx
@@ -4,7 +4,15 @@ const NavbarTop = () => {
   return(
     <div id='navbar-top'>
       <div className='navbar__top__wrapper'>
-        <p>NAVBAR TOP</p>
+        <div className='navbar__top__logo'>
+          <img src={process.env.PUBLIC_URL + '/assets/icons/terminal-solid.svg'} alt='logo'/>
+        </div>
+        <div className='navbar__top__info'>
+          <div className='navbar__top__language__switch'>
+            <div id='lang-pl' className='navbar__top__language__pl clickedPl'>PL</div>
+            <div id='lang-eng' className='navbar__top__language__eng'>EN</div>
+          </div>
+        </div>
       </div>
     </div>
   )
